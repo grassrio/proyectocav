7e908a4c434db13410dfb1c80d98dad79a1d7063
cabe23 <etchemendymartin@gmail.com>
2017-06-19 19:00:19 -0300

    prueba

---
+++ Ejemplo/Ejemplo/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace Ejemplo.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ Ejemplo/Ejemplo/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Ejemplo.Startup))]
+namespace Ejemplo
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
