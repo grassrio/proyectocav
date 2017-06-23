b9ebfa7f81611b2e037cea143062f8931bdd5c6d
grassrio <santiagopg@gmail.com>
2017-06-23 19:18:45 -0300

    Principio

---
+++ CAvanzadas/CAvanzadas/index.php
@@ -0,0 +1,21 @@
+﻿<?php
+
+echo "PHP! <br />";
+
+?>
+
+
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+</head>
+<body>
+    Usuario:
+    <input id="Text1" type="text" value="" />
+    <br />
+    Contraseña:
+    <input id="Password1" type="password" value="" />
+    <br />
+    <input id="Button1" type="button" value="Ingresar" />
+</body>
+</html>
