4ee03101d82eb0e96668492e78574d75e4cafb29
grassrio <santiagopg@gmail.com>
2017-06-24 11:03:14 -0300

    Agrego estilos, css, iconos, etc, y un los textos de login

--- CAvanzadas/CAvanzadas/index.php
+++ CAvanzadas/CAvanzadas/index.php
@@ -1,21 +1,42 @@
 ﻿<?php
 
-echo "PHP! <br />";
+echo "<br />";
 
 ?>
 
 
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>CAvanzadas</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet" />
+    <link href="css/todc-bootstrap.min.css" rel="stylesheet" />
 </head>
 <body>
-    Usuario:
-    <input id="Text1" type="text" value="" />
-    <br />
-    Contraseña:
-    <input id="Password1" type="password" value="" />
-    <br />
-    <input id="Button1" type="button" value="Ingresar" />
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+
+
+
+
+    <div class="panel panel-info">
+        <div class="panel-heading">
+            <h3 class="panel-title">Construcciones Avanzadas SRL</h3>
+        </div>
+        <div class="panel-body">
+            <div class="input-group">
+                <input type="text" class="form-control" placeholder="Usuario" aria-describedby="basic-addon2" />
+            </div>
+            <div class="input-group">
+                <input type="password" class="form-control" placeholder="Contraseña" aria-describedby="basic-addon2" />
+            </div>
+            <p>
+                <a class="btn btn-primary btn-lg" href="#" role="button">Iniciar sesión</a>
+            </p>
+        </div>
+    </div>
 </body>
 </html>
