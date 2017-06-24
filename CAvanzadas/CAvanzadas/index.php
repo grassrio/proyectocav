03868d015e7b016110233735551a19217e5ebaf5
cabe23 <etchemendymartin@gmail.com>
2017-06-24 13:44:17 -0300

    24/06

--- CAvanzadas/CAvanzadas/index.php
+++ CAvanzadas/CAvanzadas/index.php
@@ -1,5 +1,5 @@
-﻿<?php
-require('config.php');
+<?php
+require('includes/config.php');
 $link = mysql_connect($mysqlserver, $mysqluser, $mysqlpass) or die('No se pudo conectar: ' . mysql_error());
 mysql_select_db($mysqldb) or die('No se pudo seleccionar la base de datos');
 
@@ -42,23 +42,28 @@ mysql_close($link);
 
 
 
-
-
-    <div class="panel panel-info">
-        <div class="panel-heading">
-            <h3 class="panel-title">Construcciones Avanzadas SRL</h3>
-        </div>
-        <div class="panel-body">
-            <div class="input-group">
-                <input type="text" class="form-control" placeholder="Usuario" aria-describedby="basic-addon2" />
+    <form method="POST" action="">
+        <div class="panel panel-info">
+            <div class="panel-heading">
+                <h3 class="panel-title">Construcciones Avanzadas SRL</h3>
             </div>
-            <div class="input-group">
-                <input type="password" class="form-control" placeholder="Contraseña" aria-describedby="basic-addon2" />
+            <div class="panel-body">
+                <div class="input-group">
+                    <input name="user" type="text" class="form-control" placeholder="Usuario" aria-describedby="basic-addon2" />
+                </div>
+                <div class="input-group">
+                    <input name="pass" type="password" class="form-control" placeholder="Contraseña" aria-describedby="basic-addon2" />
+                </div>
+                <p>
+                    <a class="btn btn-primary btn-lg" href="#" role="button" name="login" type="submit">Iniciar sesión</a>
+                </p>
             </div>
-            <p>
-                <a class="btn btn-primary btn-lg" href="#" role="button">Iniciar sesión</a>
-            </p>
         </div>
-    </div>
+    </form>
+    <?php
+    if(isset($_POST['submit'])&&!empty($_POST['pass'])&&!empty($_POST['user'])){
+        require("includes/login_usuario.php");
+    }
+    ?>
 </body>
 </html>
