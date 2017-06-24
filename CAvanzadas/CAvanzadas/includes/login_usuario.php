50426cf5f37335eb2cbc267bcb4e408709074db3
cabe23 <etchemendymartin@gmail.com>
2017-06-24 13:52:53 -0300

    login

--- CAvanzadas/CAvanzadas/includes/login_usuario.php
+++ CAvanzadas/CAvanzadas/includes/login_usuario.php
@@ -24,7 +24,7 @@ if(isset($_POST['submit'])){
         $_SESSION['tipoUsuario']=$line;
         $_SESSION['user']=$username;
 
-		echo'session establecida!!!';
+		echo'session establecida!';
 	}
   }
 ?>
