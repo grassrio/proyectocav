bacc13c5ccb88229ecf34082e1640e9f070b0714
grassrio <santiagopg@gmail.com>
2017-06-24 12:40:06 -0300

    primera conexion a la base de datos

---
+++ CAvanzadas/CAvanzadas/config.php
@@ -0,0 +1,6 @@
+<?php
+$mysqlserver = "sql105.byethost11.com";
+$mysqluser = "b11_20285343";
+$mysqlpass = "construccionesa1";
+$mysqldb = "b11_20285343_cav";
+?>
--- CAvanzadas/CAvanzadas/index.php
+++ CAvanzadas/CAvanzadas/index.php
@@ -1,7 +1,28 @@
 ﻿<?php
+require('config.php');
+$link = mysql_connect($mysqlserver, $mysqluser, $mysqlpass) or die('No se pudo conectar: ' . mysql_error());
+mysql_select_db($mysqldb) or die('No se pudo seleccionar la base de datos');
 
-echo "<br />";
+// Realizar una consulta MySQL
+$query = 'SELECT * FROM Usuario';
+$result = mysql_query($query) or die('Consulta fallida: ' . mysql_error());
 
+// Imprimir los resultados en HTML
+echo "<table>\n";
+while ($line = mysql_fetch_array($result, MYSQL_ASSOC)) {
+    echo "\t<tr>\n";
+    foreach ($line as $col_value) {
+        echo "\t\t<td>$col_value</td>\n";
+    }
+    echo "\t</tr>\n";
+}
+echo "</table>\n";
+
+// Liberar resultados
+mysql_free_result($result);
+
+// Cerrar la conexión
+mysql_close($link);
 ?>
 
 
