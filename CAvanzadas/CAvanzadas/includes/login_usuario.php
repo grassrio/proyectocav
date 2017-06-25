0642ed2db674baabf2697dc6c0c474c378df26cd
grassrio <santiagopg@gmail.com>
2017-06-25 19:18:27 -0300

    Revert "a"
    This reverts commit 03d29b031b4c52870f29cec0227b10555bbfc54c.

--- CAvanzadas/CAvanzadas/includes/login_usuario.php
+++ CAvanzadas/CAvanzadas/includes/login_usuario.php
@@ -1,28 +1,31 @@
-?<?php
- if(isset($_POST['submit'])){
-     require('config.php');
-     $connect = mysqli_connect($mysqlserver,$mysqluser,$mysqlpass) or die('Error al conectarse a la base de datos');
-     if ($connect)
-     {
-         mysqli_select_db($connect,$mysqldb);
-         $username = $_POST['usuario'];
-         $password = $_POST['password'];
-         $sql = mysqli_query($connect,"SELECT * FROM Usuario WHERE NombreUsuario='$username' AND Password='$password'")
-              or die ("Error al consultar usuarios");
-         $rowcount = mysqli_num_rows($sql);
-         if ($rowcount<1){
-             echo 'Usuario o contraseña incorrecta';
-             mysqli_close($connect);
-         }else{
-             session_start();
-             $sql = mysqli_query($connect,"SELECT TipoUsuario FROM Usuario WHERE NombreUsuario='$username'")
-             or die ("Error al consultar el tipo de usuario");
+<?php
+session_start();
 
-             $tipousuario = mysqli_fetch_array($sql);
-             $_SESSION['usuario']=$username;
-             $_SESSION['tipoUsuario']=$tipousuario;
-             header("location:../index.php");
-         }
-     }
- }
- ?>
+if(isset($_POST['submit'])){
+    require('config.php');
+    $connect = mysqli_connect($mysqlserver,$mysqluser,$mysqlpass) or die('No se pudo conectar:');
+    if ($connect)
+    {
+        mysqli_select_db($connect,$mysqldb);
+        $username = $_POST['user'];
+        $password = $_POST['pass'];
+        $sql = mysqli_query($connect,"SELECT * FROM Usuario WHERE NombreUsuario='$username' AND Password='$password'")
+             or die ("Faild");
+        $rowcount = mysqli_num_rows($sql);
+        if ($rowcount<1){
+            echo 'Usuario o password incorrectas';
+            mysqli_close($connect);
+        }else{
+            $sql = mysqli_query($connect,"SELECT TipoUsuario FROM Usuario WHERE NombreUsuario='$username'")
+            or die ("Faild");
+
+            $line = mysqli_fetch_array($sql);
+
+            $_SESSION['tipoUsuario']=$line;
+            $_SESSION['user']=$username;
+
+            echo'session establecida!';
+        }
+    }
+}
+?>
--- CAvanzadas/CAvanzadas/index.php
+++ CAvanzadas/CAvanzadas/index.php
@@ -1,7 +1,30 @@
 <?php
-require('includes/loginheader.php');
-session_start();
-if (isset($_SESSION['usuario'])) { ?>
+require('includes/config.php');
+$link = mysql_connect($mysqlserver, $mysqluser, $mysqlpass) or die('No se pudo conectar: ' . mysql_error());
+mysql_select_db($mysqldb) or die('No se pudo seleccionar la base de datos');
+
+// Realizar una consulta MySQL
+$query = 'SELECT * FROM Usuario';
+$result = mysql_query($query) or die('Consulta fallida: ' . mysql_error());
+
+// Imprimir los resultados en HTML
+echo "<table>\n";
+while ($line = mysql_fetch_array($result, MYSQL_ASSOC)) {
+    echo "\t<tr>\n";
+    foreach ($line as $col_value) {
+        echo "\t\t<td>$col_value</td>\n";
+    }
+    echo "\t</tr>\n";
+}
+echo "</table>\n";
+
+// Liberar resultados
+mysql_free_result($result);
+
+// Cerrar la conexión
+mysql_close($link);
+?>
+
 
 <!DOCTYPE html>
 <html lang="en">
@@ -17,9 +40,31 @@ if (isset($_SESSION['usuario'])) { ?>
     <script src="js/jquery.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
 
-    <a class="btn btn-primary" href="includes/logout.php" role="button" name="login" type="submit">Cerrar sesión</a>
+
+
+    <form method="POST" action="">
+        <div class="panel panel-info">
+            <div class="panel-heading">
+                <h3 class="panel-title">Construcciones Avanzadas SRL</h3>
+            </div>
+            <div class="panel-body">
+                <div class="input-group">
+                    <input name="user" type="text" class="form-control" placeholder="Usuario" aria-describedby="basic-addon2" />
+                </div>
+                <div class="input-group">
+                    <input name="pass" type="password" class="form-control" placeholder="Contraseña" aria-describedby="basic-addon2" />
+                </div>
+                <p>
+                    <input class="btn btn-primary btn-lg"  name="submit" type="submit" id="btn" value="ingresar" />
+                  <!--  <a class="btn btn-primary btn-lg" href="#" role="button" name="login" type="submit">Iniciar sesión</a>-->
+                </p>
+            </div>
+        </div>
+    </form>
+    <?php
+    if(isset($_POST['submit'])&&!empty($_POST['pass'])&&!empty($_POST['user'])){
+        require("includes/login_usuario.php");
+    }
+    ?>
 </body>
 </html>
-
-<?php } ?>
-
