24c99f86d909a6b2a595cc913b161638d13ce713
grassrio <santiagopg@gmail.com>
2017-06-25 17:23:10 -0300

    base de datos de nuevo hosting que permite remoto

--- CAvanzadas/CAvanzadas/includes/config.php
+++ CAvanzadas/CAvanzadas/includes/config.php
@@ -1,19 +1,14 @@
 <?php
-//para publicar
-$mysqlserver = "sql105.byethost11.com";
-$mysqluser = "b11_20285343";
-$mysqlpass = "construccionesa1";
-$mysqldb = "b11_20285343_cav";
+//Siempre, nuevo host que permite remoto
+$mysqlserver = "64.62.211.131";
+$mysqluser = "cavuser_dbuser";
+$mysqlpass = "Dbpass1";
+$mysqldb = "cavuser_db";
 
-/* local 
+/* local
 $mysqlserver = "localhost";
 $mysqluser = "root";
 $mysqlpass = "";
 $mysqldb = "login";
 
-base de datos compartida sin publicar
-$mysqlserver = "sql10.freesqldatabase.com";
-$mysqluser = "sql10181897";
-$mysqlpass = "xxb6iJVXil";
-$mysqldb = "sql10181897";*/
 ?>
