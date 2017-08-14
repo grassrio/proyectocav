65d096b07a065ffd38765e23531bd633d10909b4
grassrio <santiagopg@gmail.com>
2017-08-14 17:38:58 -0300

    saco clase baliza

--- CAvanzadas/CAvanzadas/includes/ConsultasBaliza.php
+++ CAvanzadas/CAvanzadas/includes/ConsultasBaliza.php
@@ -1,27 +1,26 @@
 <?php
-require 'Clases/Baliza.php';
 function DevolverBalizasProximoVencimiento(){
     require('config.php');
     $connect = mysqli_connect($mysqlserver,$mysqluser,$mysqlpass) or die('Error al conectarse a la base de datos');
     if ($connect)
     {
         mysqli_select_db($connect,$mysqldb);
-        $baliza = new Baliza();
-        $sql = $baliza->BalizasProximoVencimiento($connect);
+        $sql = mysqli_query($connect,"SELECT * FROM Baliza WHERE (DATEDIFF(fechaFin, NOW( )) <=2) AND fechaDevolucion IS NULL")
+           or die ("Error al consultar Baliza");
         mysqli_close($connect);
         return $sql;
     }
     return $sql;
 }
-function devolverBaliza($id)
+function devolverBaliza($idBaliza)
 {
     require('config.php');
     $connect = mysqli_connect($mysqlserver,$mysqluser,$mysqlpass) or die('Error al conectarse a la base de datos');
     if ($connect)
     {
         mysqli_select_db($connect,$mysqldb);
-        $baliza = new Baliza();
-        $sql = $baliza->devolverBalizas($connect,$id);
+        $sql = mysqli_query($connect,"UPDATE Baliza SET fechaDevolucion=NOW() WHERE idBaliza='".$idBaliza."'")
+           or die ("Error al devolver Baliza");
         mysqli_close($connect);
         return $sql;
     }
