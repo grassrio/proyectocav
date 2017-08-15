2aa4f3822c8401714d6b8dedce793bda8f38d670
grassrio <santiagopg@gmail.com>
2017-08-15 02:48:12 -0300

    elimino clase personal

--- CAvanzadas/CAvanzadas/includes/ConsultasPersonal.php
+++ CAvanzadas/CAvanzadas/includes/ConsultasPersonal.php
@@ -1,5 +1,4 @@
 <?php
-require 'Clases/Personal.php';
 
 function devolverPersonal(){
     require('config.php');
@@ -7,12 +6,13 @@ function devolverPersonal(){
     if ($connect)
     {
         mysqli_select_db($connect,$mysqldb);
-        $personal = new Personal();
-        $sql = $personal->devolverPersonal($connect);
+        $sql = mysqli_query($connect,"SELECT * FROM Personal") or die ("Error al obtener Personal");
+        mysqli_close($connect);
         return $sql;
     }
     return $sql;
 }
+
 function insertarPersonal($nombreCompleto,$direccion,$telefono,$cargo)
 {
     require('config.php');
@@ -20,8 +20,9 @@ function insertarPersonal($nombreCompleto,$direccion,$telefono,$cargo)
     if ($connect)
     {
         mysqli_select_db($connect,$mysqldb);
-        $personal = new Personal();
-        $sql = $personal->InsertarPersonal($connect,$nombreCompleto,$direccion,$telefono,$cargo);
+        $sql = mysqli_query($connect,"INSERT INTO Personal (NombreCompleto,Direccion,Telefono,Cargo) VALUES ('".$nombreCompleto."','".$direccion."','".$telefono."','".$cargo."')")
+            or die ("Error al insertar personal");
+        mysqli_close($connect);
         return $sql;
 
     }
@@ -35,8 +36,9 @@ function modificarPersonal($id,$direccion,$telefono,$cargo)
     if ($connect)
     {
         mysqli_select_db($connect,$mysqldb);
-        $personal = new Personal();
-        $sql = $personal->ModificarPersonal($connect,$id,$direccion,$telefono,$cargo);
+        $sql = mysqli_query($connect,"UPDATE Personal SET Direccion='".$direccion."',Telefono='".$telefono."', Cargo='".$cargo."' WHERE idPersonal='".$id."'")
+            or die ("Error al modificar personal");
+        mysqli_close($connect);
         return $sql;
     }
     return $sql;
@@ -49,8 +51,9 @@ function eliminarPersonal($NombreCompleto)
     if ($connect)
     {
         mysqli_select_db($connect,$mysqldb);
-        $personal = new Personal();
-        $sql = $personal->EliminarPersonal($connect,$NombreCompleto);
+        $sql = mysqli_query($connect,"DELETE FROM Personal WHERE NombreCompleto='".$NombreCompleto."'")
+         or die ("Error al eliminar personal");
+        mysqli_close($connect);
         return $sql;
     }
     return $sql;
@@ -62,8 +65,8 @@ function devolverObrero($nombreCompleto)
     if ($connect)
     {
         mysqli_select_db($connect,$mysqldb);
-        $personal = new Personal();
-        $sql = $personal->devolverObrero($connect,$nombreCompleto);
+        $sql = mysqli_query($connect,"SELECT * FROM Personal WHERE NombreCompleto='".$nombreCompleto."'") or die ("Error al consultar personal");
+        mysqli_close($connect);
         return $sql;
     }
     return $sql;
@@ -75,8 +78,8 @@ function devolverObreros()
     if ($connect)
     {
         mysqli_select_db($connect,$mysqldb);
-        $personal = new Personal();
-        $sql = $personal->devolverObreros($connect);
+        $sql = mysqli_query($connect,"SELECT * FROM Personal WHERE Cargo ='Obrero'") or die ("Error al consultar personal");
+        mysqli_close($connect);
         return $sql;
     }
     return $sql;
@@ -89,8 +92,8 @@ function obtenerObreroPorId($idObrero)
     if ($connect)
     {
         mysqli_select_db($connect,$mysqldb);
-        $personal = new Personal();
-        $sql  = $personal->DevolverUnPersonal($connect,$idObrero);
+        $sql = mysqli_query($connect,"SELECT * FROM Personal WHERE idPersonal='".$idObrero."'") or die ("Error al consultar personal");
+        mysqli_close($connect);
         return $sql;
     }
     return $sql;
@@ -102,9 +105,9 @@ function ObrasFinalizadasEntreFecha($fechaInicio,$fechaFin)
     if ($connect)
     {
         mysqli_select_db($connect,$mysqldb);
-        $personal = new Personal();
         $sql = mysqli_query($connect,"SELECT pc.Nombre,SUM(mo.MetrajeReal) FROM PersonalCuadrilla pc, Obra o, MetrajeObra mo WHERE mo.idObra=o.idObra AND o.fechaFinalizado>='".$fechaInicio."' AND o.fechaFinalizado<='".$fechaFin."' AND o.idCuadrilla=pc.idCuadrilla AND mo.MetrajeReal>0 GROUP BY pc.Nombre")
          or die ("Error al calcular la productividad");
+        mysqli_close($connect);
         return $sql;
     }
     return $sql;
@@ -116,9 +119,9 @@ function MostrarProductividadPorObrero($nombreEmpleado,$fechaInicio,$fechaFin)
     if ($connect)
     {
         mysqli_select_db($connect,$mysqldb);
-        $personal = new Personal();
         $sql = mysqli_query($connect,"SELECT mo.MetrajeReal,mo.NombreRubro,mo.Unidad,rc.Precio,pc.Porcentaje FROM RubroCotizacion rc,PersonalCuadrilla pc, Obra o, MetrajeObra mo WHERE mo.idObra=o.idObra AND o.idCuadrilla=pc.idCuadrilla AND pc.Nombre='".$nombreEmpleado."' AND  o.fechaFinalizado>='".$fechaInicio."' AND o.fechaFinalizado<='".$fechaFin."'AND rc.nombreRubro=mo.NombreRubro AND mo.MetrajeReal>0 and rc.idCotizacion=o.idCotizacion")
          or die ("Error al calcular la productividad");
+        mysqli_close($connect);
         return $sql;
     }
     return $sql;
